bf2f4290ca9612a84ea4d617245b6335a6097b3c
Truong Cong Danh <truongcongdanh2736@gmail.com>
2026-05-02 07:27:56 +0700

    change link

--- interacthub-web/src/api/axiosClient.ts
+++ interacthub-web/src/api/axiosClient.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 //const API_BASE_URL = 'https://localhost:7275/api';
-//const API_BASE_URL = 'https://interacthub-api.azurewebsites.net/api';
-const API_BASE_URL = 'https://congdanh2703-001-site1.stempurl.com/api';
+const API_BASE_URL = 'https://interacthub.azurewebsites.net/api';
+//const API_BASE_URL = 'https://congdanh2703-001-site1.stempurl.com/api';
 
 
 const axiosClient = axios.create({
